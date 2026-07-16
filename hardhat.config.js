b92e161baaa8bf1ac5c17b77ab15326a434094c9
Mai Mai <sn6024010087@camtech.edu.kh>
2026-07-16 19:43:00 +0700

    updated-metamaskConnection

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,22 @@
-require("@nomicfoundation/hardhat-toolbox");
+import { defineConfig } from "hardhat/config";
+import hardhatToolboxMochaEthers from "@nomicfoundation/hardhat-toolbox-mocha-ethers";
 
-/** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
+export default defineConfig({
+  plugins: [
+    hardhatToolboxMochaEthers
+  ],
   solidity: "0.8.20",
   networks: {
     localhost: {
+      type: "http",
       url: "http://127.0.0.1:8545"
     },
     sepolia: {
-      url: process.env.SEPOLIA_RPC_URL || "",
+      type: "http",
+      url: process.env.SEPOLIA_RPC_URL || "http://127.0.0.1:8545",
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
     }
   }
-};
+});
+
+
---
+++ scripts/test_hre.js
@@ -0,0 +1,2 @@
+import hardhatToolboxMochaEthers from "@nomicfoundation/hardhat-toolbox-mocha-ethers";
+console.log("Imported plugin:", hardhatToolboxMochaEthers);
